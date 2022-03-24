e117179a3647d040c249844a0d957d4bd95654f1
Eugene Kos <mstrsid@live.com>
2022-03-24 10:54:24 +0300

    done

---
+++ src/js/lib/components/accordion.js
@@ -0,0 +1,17 @@
+import $ from '../core';
+
+$.prototype.accordion = function (headActiveClass = 'accordion-head__active', contentActiveClass = 'accordion-content__active', paddings = 40) {
+	for (let i = 0; i < this.length; i++) {
+		$(this[i]).click(() => {
+			$(this[i]).toggleClass(headActiveClass);
+			$(this[i].nextElementSibling).toggleClass(contentActiveClass);
+			if (this[i].classList.contains(headActiveClass)) {
+				this[i].nextElementSibling.style.maxHeight = this[i].nextElementSibling.scrollHeight + paddings + 'px';
+			} else {
+				this[i].nextElementSibling.style.maxHeight = '0px';
+			}
+		});
+	}
+};
+
+$('.accordion-head').accordion();
